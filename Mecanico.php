c07b59f9c9cd4d2e117e04a0d164a0637d7a157c
GozalezAVictor <glezvic96@live.com>
2016-11-13 23:21:20 -0600

    Agregado el mecanico funcinando con el metodo calcular_desgarste()

---
+++ Mecanico.php
@@ -0,0 +1,55 @@
+<?php
+
+	class Mecanico{
+
+		function __construct(){
+
+		}
+
+		public function calcular_desgaste($componente, $tiempo_uso_equipo){
+
+			echo "calcular_desgaste() en Mecanico<br>";
+
+			$porcentaje_uso_componente = $componente->obtener_porcentaje_uso();
+
+			print('procentaje uso componente: '.$porcentaje_uso_componente.'<br>');
+
+			$tiempo_usado = $porcentaje_uso_componente * $tiempo_uso_equipo;
+
+			// $id_componente = $componente->obtener_id();
+
+			// $nombre_componente = $componente->obtener_nombre();
+
+			// $descripcion_componente = $componente->obtener_descripcion();
+
+			// $tiempo_vida_max = $componente->obtener_tiempo_vida_max();
+
+			$tiempo_vida_actual = $componente->obtener_tiempo_vida_actual();
+ 
+			$tiempo_vida_actual = $tiempo_vida_actual + $tiempo_usado;
+
+			// print('id: '.$componente->obtener_id().' -> '.$componente->obtener_tiempo_vida_actual().'<br>');
+
+			$componente->modificar_tiempo_vida_actual($tiempo_vida_actual);
+
+			// print('id: '.$componente->obtener_id().' -> '.$componente->obtener_tiempo_vida_actual().'<br>');
+
+			// $datos_componente = array('id' => $id_componente ,
+			//                         'nombre' => $nombre_componente,
+			//                         'descripcion' => $descripcion_componente,
+			//                         'tiempo_vida_max' => $tiempo_vida_max,
+			//                         'tiempo_vida_actual' => $tiempo_vida_actual,
+			//                         'porcentaje_uso' => $porcentaje_uso_componente);
+
+			// $componente_usado = new Componente($datos_componente);
+
+			// $componentes_despues_proceso[] = $componente_usado;
+
+		}
+
+
+	}
+
+
+
+?>
