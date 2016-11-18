f0be53b8f7970bda1af4b0bf9c9b018e050833c2
Raul Canul <eduardoct120@gmail.com>
2016-11-18 17:23:30 -0600

    Se refactorizaron algunas cosas de la clase

--- Asignador_tareas.php
+++ Asignador_tareas.php
@@ -4,80 +4,79 @@
   include 'Administrador_equipo.php';
   include 'Administrador_componente.php';
   include 'Simulador_procesos.php';
-
-  //Se recive la tarea del usuario y se la pasa a un Asignador_tareas
-
-  $tarea_usuario = $_POST[ 'tarea' ];
-  $datos_tarea = $_POST[ 'datos' ];
-
-  $asignador_tareas = new Asignador_tareas( $tarea_usuario, $datos_tarea );
-
-  //----------------------------------------------------------------------------
-  // CLASE ASIGNADOR DE TAREAS
-  //
-  // Esta clase esta encargadas de manejar la tarea que el usuario quiere hacer
-  //----------------------------------------------------------------------------
-
+  /*Clase que sirve como delegador de lo que el usuario quiere hacer*/
   class Asignador_tareas {
 
     private $tarea;
     private $datos_tarea;
-    private $administrador;
-    private $simulador_procesos;
+    private $encargado_tarea;
 
+    /*
+     *Construct
+     *@param String $tarea - contiene la tarea que el usuario quiere hacer
+     *@param Array $datos_tarea - contiene todos los datos para realizar la tarea
+     */
     function __construct( $tarea, $datos_tarea ) {
       $this->tarea = $tarea;
       $this->datos_tarea = $datos_tarea;
 
-      $this->asignar_administrador( $this->datos_tarea[ 'tipo_elemento' ] );
+      $this->encontrar_encargado_tarea( $this->datos_tarea[ 'tipo_elemento' ] );
       $this->asignar_tarea();
     }
 
-    //Método que selecciona el tipo de administrador a utilizar.
 
-    public function asignar_administrador( $tipo_elemento ) {
+    /*
+     *Encuentra quien deberia hacer la tarea segun el elemento sobre quien es la tarea
+     *@param String $tipo_elemento - Contiene el nombre del elemento sobre quien se va a hacer la tarea
+     *@return void
+     */
+    public function encontrar_encargado_tarea( $tipo_elemento ) {
       switch( $tipo_elemento ){
         case 'procesos':
-          $this->administrador = new Administrador_proceso();
+          $this->encargado_tarea = new Administrador_proceso();
           break;
 
         case 'equipos':
-          $this->administrador = new Administrador_equipo();
+          $this->encargado_tarea = new Administrador_equipo();
           break;
 
         case 'componentes':
-          $this->administrador = new Administrador_componente();
+          $this->encargado_tarea = new Administrador_componente();
           break;
 
         case 'simulador':
-          $this->simulador_procesos = new Simulador_procesos();
+          $this->encargado_tarea = new Simulador_procesos();
       }
     }
 
+    /*
+     *Asigna la tarea del usuario al quien la deberia realizar
+     *@return void
+     */
     public function asignar_tarea() {
       switch ( $this->tarea ) {
         case 'agregar':
-          $this->administrador->agregar_nuevo( $this->datos_tarea );
+          $this->encargado_tarea->agregar_nuevo( $this->datos_tarea );
         break;
 
         case 'modificar':
-          $this->administrador->modificar( $this->datos_tarea );
+          $this->encargado_tarea->modificar( $this->datos_tarea );
         break;
 
         case 'eliminar':
-          $this->administrador->eliminar( $this->datos_tarea );
+          $this->encargado_tarea->eliminar( $this->datos_tarea );
         break;
 
         case 'generar historial':
-          $this->administrador->generar_historial();
+          $this->encargado_tarea->generar_historial();
         break;
 
         case 'simular':
-          $this->simulador_procesos->simular( $procesos );
+          $this->encargado_tarea->simular( $procesos );
         break;
 
         case 'activar proceso':
-          $this->administrador->iniciar_proceso( $id_proceso );
+          $this->encargado_tarea->iniciar_proceso( $id_proceso );
         break;
 
         default:
@@ -88,5 +87,17 @@
     }
 
   }
+
+/**
+ *
+ */
+function Main() {
+    $tarea_usuario = $_POST[ 'tarea' ];
+    $datos_tarea = $_POST[ 'datos' ];
+
+    $asignador_tareas = new Asignador_tareas( $tarea_usuario, $datos_tarea );
+  }
+
+  Main();
 
 ?>
