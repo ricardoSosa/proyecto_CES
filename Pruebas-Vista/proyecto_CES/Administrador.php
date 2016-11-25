4a7aefa3c95f4a768b3a207682ed29bae497e140
Raul Canul <eduardoct120@gmail.com>
2016-11-25 16:21:29 -0600

    Pruebas de integración con las vistas

---
+++ Pruebas-Vista/proyecto_CES/Administrador.php
@@ -0,0 +1,34 @@
+<?php
+
+  include_once "Manejador_base_datos.php";
+
+  abstract class Administrador {
+
+    private $manejador_bd;
+
+    function __construct() {
+      $this->manejador_bd = new Manejador_base_datos();
+    }
+
+    public function agregar_nuevo( $datos ) {
+      $tipo_elemento = $datos[ 'tipo_elemento' ];
+
+      $this->manejador_bd->insertar( $tipo_elemento, $datos );
+    }
+
+    public function eliminar( $tipo_elemento, $id ) {
+      $this->manejador_bd->eliminar( $tipo_elemento, $id );
+    }
+
+    public function leer_datos( $datos ) {
+      $tipo_elemento = $datos[ 'tipo_elemento' ];
+
+      $this->manejador_bd->realizar_consulta( $tipo_elemento, $datos );
+    }
+
+    public function modificar( $tipo_elemento, $datos ) {
+      $this->manejador_bd->modificar( $tipo_elemento, $datos  );
+    }
+
+  }
+?>
---
+++ Pruebas-Vista/proyecto_CES/Administrador_componente.php
@@ -0,0 +1,13 @@
+<?php
+
+  include_once "Administrador.php";
+
+  class Administrador_componente extends Administrador {
+
+    function __construct() {
+      parent::__construct();
+    }
+
+  }
+
+?>
